843a626b145fb5752b9bc7f4b37a60eeefa09226
BS\Bartosz <staniszbartosz@gmail.com>
2019-08-16 22:56:05 +0200

    Init commit

---
+++ src/main/java/moje/flywaytutorial/FlywayTutorialApplication.java
@@ -0,0 +1,13 @@
+package moje.flywaytutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayTutorialApplication.class, args);
+	}
+
+}
